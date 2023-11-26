b40426a862b5ecbcabd840bcadc8caa9bedb91c1
jazz-y <112431546+jazz-y@users.noreply.github.com>
2023-11-26 13:20:43 -0600

    Reworked drawing the islands, Sam not showing

--- finalgame.asm
+++ finalgame.asm
@@ -16,8 +16,26 @@ BLUE = #$AC
 
 ; height is the true height - 1.
 SAM_HEIGHT = #21
-ISLAND_HEIGHT = #9
+ISLAND_HEIGHT = #24
 SAM_INITIAL_Y = #25
+ISLAND_ROWS = #3
+SPACER_HEIGHT = #19
+; for now:
+SAM_RANGE = #200
+
+TOP_SPACER_HEIGHT = #5
+
+ISLAND1_HEIGHT = #20 ; where island1 starts
+ISLAND1_SPACER_HEIGHT = #10 ; where island1 spacer starts
+
+ISLAND2_HEIGHT = #20 ; where island2 starts
+ISLAND2_SPACER_HEIGHT = #10 ; where island1 spacer starts
+
+ISLAND3_HEIGHT = #20 ; where island3 starts
+ISLAND3_SPACER_HEIGHT = #10 ; where island1 spacer starts
+
+ISLAND4_HEIGHT = #20 ; where island4 starts
+ISLAND4_SPACER_HEIGHT = #10 ; where island1 spacer starts
 
 ;------------------------------------------------
 ; RAM
@@ -33,8 +51,15 @@ samgfx			.byte
 
 samcolor		.byte
 samY 			.byte
+samrange 		.byte
 
-drawsam
+drawsam			.byte
+
+; island graphics stuff
+drawisland		.byte
+islandrows		.byte
+
+spacerheight	.byte
 
 	echo [(* - $80)]d, " RAM bytes used"
 
@@ -62,6 +87,9 @@ Start
 	
 	lda 	#SAM_INITIAL_Y
 	sta 	samY
+	
+	lda 	#0
+	sta 	PF0
 
 ;------------------------------------------------
 ; Vertical Blank
@@ -84,24 +112,76 @@ MainLoop
 	lda 	#%00000001
 	sta 	CTRLPF
 	
+.checkReset
+	lda 	#%00000001
+	bit 	SWCHB
+	bne		Next
+	jmp		Start
+	
+Next
+
+CheckJoyUp
+	lda 	#%00010000
+	bit 	SWCHA
+	bne 	.endCheckJoyUp
+	lda 	samY
+	cmp 	#SAM_RANGE-1
+	beq		.endCheckJoyUp
+	inc 	samY
+	inc 	samY
+.endCheckJoyUp
+CheckJoyDown
+	lda 	#%00100000
+	bit 	SWCHA
+	bne 	.endCheckJoyDown
+	lda 	samY
+	cmp 	#SAM_HEIGHT+2 ; idea: check to see if sam has reached bottom limit
+	beq		.endCheckJoyDown
+	dec 	samY
+	dec 	samY
+.endCheckJoyDown
+CheckJoyLeft
+	lda 	#%01000000
+	bit 	SWCHA
+	bne 	.endCheckJoyLeft
+	lda 	#%00001000
+	sta 	REFP0
+	lda 	#%00100000 ; horizontal motion from table (stella guide)
+	sta 	HMP0	   ; put in HMP0, strobe HMOVE after WSYNC to activate
+.endCheckJoyLeft
+CheckJoyRight
+	lda		#%10000000
+	bit 	SWCHA
+	bne 	.endCheckJoyRight
+	sta 	REFP0
+	lda 	#%11100000
+	sta 	HMP0
+.endCheckJoyRight
+	
 .waitForVBlank
 	lda		INTIM
 	bne		.waitForVBlank
 	sta		WSYNC
 	sta		VBLANK
+	sta 	HMOVE ; strobing HMOVE
 
 ;------------------------------------------------
 ; Kernel
 ;------------------------------------------------	
 DrawScreen
 ;	ldx		#192+1		 Kernel goes here
-	ldx 	#45+1
+	lda 	#SAM_RANGE
+	sta 	samrange
+	ldx 	#TOP_SPACER_HEIGHT
 ; DISCLAIMER: make the islands part of the PF, add sprites as the island disasters.
 .scanline
+	;cpx 	#SAM_RANGE
+	;bne 	.checkIsland1
+	ldx 	#TOP_SPACER_HEIGHT
+.topSpacer
 	lda		bgcolor
 	sta		COLUBK
-	lda		#0 
-	sta 	PF0
+	lda		#0 		; not storing into PF0 - always 0, never changed
 	sta 	PF1
 	sta 	PF2
 	
@@ -110,6 +190,311 @@ DrawScreen
 	lda 	samcolor
 	sta 	COLUP0
 	
+.startCheckSam1
+	; does sam start on this scan line?
+	cpx 	samY
+	bne 	.loadSam1
+	
+	lda 	#SAM_HEIGHT
+	sta 	drawsam
+.loadSam1
+	lda 	drawsam
+	cmp 	#$FF ; comparing to FF because when you decrement 0 it goes to FF
+	beq 	.noSam1 ; If sam is done loading, go down to .noSam
+	tay
+	lda 	(samrestinggfx),y
+	sta 	samgfx
+	lda 	SamColors,y
+	sta 	samcolor
+	
+	dec 	drawsam
+	jmp 	.endSam1
+	
+.noSam1
+	lda 	#0
+	sta 	samgfx
+.endSam1
+	
+	dex
+	sta		WSYNC
+	bne		.topSpacer
+	
+.checkIsland1
+	ldx 	#ISLAND1_HEIGHT
+	
+.drawIsland1
+	lda 	IslandSprite
+	sta 	PF1
+	sta 	PF2
+	lda 	#0
+	sta 	PF0
+	lda 	#IslandColors-1,x
+	sta 	COLUPF
+	
+;	dex
+;	sta 	WSYNC
+;	bne 	.drawIsland1
+.startCheckSam2
+	; does sam start on this scan line?
+	cpx 	samY
+	bne 	.loadSam2
+	
+	lda 	#SAM_HEIGHT
+	sta 	drawsam
+.loadSam2
+	lda 	drawsam
+	cmp 	#$FF ; comparing to FF because when you decrement 0 it goes to FF
+	beq 	.noSam2 ; If sam is done loading, go down to .noSam
+	tay
+	lda 	(samrestinggfx),y
+	sta 	samgfx
+	lda 	SamColors,y
+	sta 	samcolor
+	
+	dec 	drawsam
+	jmp 	.endSam2
+	
+.noSam2
+	lda 	#0
+	sta 	samgfx
+.endSam2
+	
+	dex
+	sta		WSYNC
+	bne		.drawIsland1
+	
+.checkSpacer1
+	ldx 	#ISLAND1_SPACER_HEIGHT
+	
+.spacer1
+	lda 	#0
+	sta		PF0
+	sta 	PF1
+	sta		PF2
+	
+;	dex
+;	sta 	WSYNC
+;	bne 	.spacer1
+
+.startCheckSam3
+	; does sam start on this scan line?
+	cpx 	samY
+	bne 	.loadSam3
+	
+	lda 	#SAM_HEIGHT
+	sta 	drawsam
+.loadSam3
+	lda 	drawsam
+	cmp 	#$FF ; comparing to FF because when you decrement 0 it goes to FF
+	beq 	.noSam3 ; If sam is done loading, go down to .noSam
+	tay
+	lda 	(samrestinggfx),y
+	sta 	samgfx
+	lda 	SamColors,y
+	sta 	samcolor
+	
+	dec 	drawsam
+	jmp 	.endSam3
+	
+.noSam3
+	lda 	#0
+	sta 	samgfx
+.endSam3
+	
+	dex
+	sta		WSYNC
+	bne		.spacer1
+	
+.checkIsland2
+	ldx 	#ISLAND2_HEIGHT
+	
+.drawIsland2
+	lda 	IslandSprite
+	sta 	PF1
+	sta 	PF2
+	lda 	#0
+	sta 	PF0
+	lda 	#IslandColors-1,x
+	sta 	COLUPF
+; add in sam stuff somewhere around here later
+;	dex
+;	sta 	WSYNC
+;	bne 	.drawIsland2
+
+.startCheckSam4
+	; does sam start on this scan line?
+	cpx 	samY
+	bne 	.loadSam4
+	
+	lda 	#SAM_HEIGHT
+	sta 	drawsam
+.loadSam4
+	lda 	drawsam
+	cmp 	#$FF ; comparing to FF because when you decrement 0 it goes to FF
+	beq 	.noSam4 ; If sam is done loading, go down to .noSam
+	tay
+	lda 	(samrestinggfx),y
+	sta 	samgfx
+	lda 	SamColors,y
+	sta 	samcolor
+	
+	dec 	drawsam
+	jmp 	.endSam4
+	
+.noSam4
+	lda 	#0
+	sta 	samgfx
+.endSam4
+	
+	dex
+	sta		WSYNC
+	bne		.drawIsland2
+	
+.checkSpacer2
+	ldx 	#ISLAND2_SPACER_HEIGHT
+
+.spacer2
+	lda 	#0
+	sta 	PF1
+	sta		PF2	
+;	dex
+;	sta 	WSYNC
+;	bne 	.spacer2
+
+.startCheckSam5
+	; does sam start on this scan line?
+	cpx 	samY
+	bne 	.loadSam5
+	
+	lda 	#SAM_HEIGHT
+	sta 	drawsam
+.loadSam5
+	lda 	drawsam
+	cmp 	#$FF ; comparing to FF because when you decrement 0 it goes to FF
+	beq 	.noSam5 ; If sam is done loading, go down to .noSam
+	tay
+	lda 	(samrestinggfx),y
+	sta 	samgfx
+	lda 	SamColors,y
+	sta 	samcolor
+	
+	dec 	drawsam
+	jmp 	.endSam5
+	
+.noSam5
+	lda 	#0
+	sta 	samgfx
+.endSam5
+	
+	dex
+	sta		WSYNC
+	bne		.spacer2
+
+.checkIsland3
+	ldx 	#ISLAND3_HEIGHT
+
+.drawIsland3
+	lda 	IslandSprite
+	sta 	PF1
+	sta 	PF2
+	lda 	#0
+	sta 	PF0
+	lda 	#IslandColors-1,x
+	sta 	COLUPF
+	
+;	dex
+;	sta 	WSYNC
+;	bne 	.drawIsland3
+; //////////////// Check Sam 6
+.startCheckSam6
+	; does sam start on this scan line?
+	cpx 	samY
+	bne 	.loadSam6
+	
+	lda 	#SAM_HEIGHT
+	sta 	drawsam
+.loadSam6
+	lda 	drawsam
+	cmp 	#$FF ; comparing to FF because when you decrement 0 it goes to FF
+	beq 	.noSam6 ; If sam is done loading, go down to .noSam
+	tay
+	lda 	(samrestinggfx),y
+	sta 	samgfx
+	lda 	SamColors,y
+	sta 	samcolor
+	
+	dec 	drawsam
+	jmp 	.endSam6
+	
+.noSam6
+	lda 	#0
+	sta 	samgfx
+.endSam6
+	
+	dex
+	sta		WSYNC
+	bne		.drawIsland3
+; ///////////////////////////////////
+
+.checkSpacer3
+	ldx 	#ISLAND3_SPACER_HEIGHT
+
+.spacer3
+	lda 	#0
+	sta		PF0
+	sta 	PF1
+	sta		PF2
+	
+;	dex
+;	sta 	WSYNC
+;	bne 	.spacer3
+
+.startCheckSam7
+	; does sam start on this scan line?
+	cpx 	samY
+	bne 	.loadSam7
+	
+	lda 	#SAM_HEIGHT
+	sta 	drawsam
+.loadSam7
+	lda 	drawsam
+	cmp 	#$FF ; comparing to FF because when you decrement 0 it goes to FF
+	beq 	.noSam7 ; If sam is done loading, go down to .noSam
+	tay
+	lda 	(samrestinggfx),y
+	sta 	samgfx
+	lda 	SamColors,y
+	sta 	samcolor
+	
+	dec 	drawsam
+	jmp 	.endSam7
+	
+.noSam7
+	lda 	#0
+	sta 	samgfx
+.endSam7
+	
+	dex
+	sta		WSYNC
+	bne		.spacer3
+
+.checkIsland4
+	ldx 	#ISLAND4_HEIGHT
+
+.drawIsland4
+	lda 	IslandSprite
+	sta 	PF1
+	sta 	PF2
+	lda 	#0
+	sta 	PF0
+	lda 	#IslandColors-1,x
+	sta 	COLUPF
+	
+;	dex
+;	sta 	WSYNC
+;	bne 	.drawIsland4
+; start checking for Sam ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.startCheckSam
 	; does sam start on this scan line?
 	cpx 	samY
 	bne 	.loadSam
@@ -132,109 +517,15 @@ DrawScreen
 .noSam
 	lda 	#0
 	sta 	samgfx
+	
 .endSam
 	
 	dex
 	sta		WSYNC
-	bne		.scanline
+	bne		.drawIsland4
 	
-	ldx 	#10
-.drawIsland1
-	lda 	IslandSprite
-	sta 	PF1
-	sta 	PF2
-	lda 	#0
-	sta 	PF0
-	lda 	#IslandColors-1,x
-	sta 	COLUPF
-	sta 	WSYNC
-	
-	dex
-	sta 	WSYNC
-	bne 	.drawIsland1
-	
-	ldx 	#10
-.spacer1
-	lda 	#0
-	sta		PF0
-	sta 	PF1
-	sta		PF2
-	sta 	WSYNC
-	
-	dex
-	sta 	WSYNC
-	bne 	.spacer1
-	
-	ldx 	#10
-.drawIsland2
-	lda 	IslandSprite
-	sta 	PF1
-	sta 	PF2
-	lda 	#0
-	sta 	PF0
-	lda 	#IslandColors-1,x
-	sta 	COLUPF
-	sta 	WSYNC
-	
-	dex
-	sta 	WSYNC
-	bne 	.drawIsland2
-	
-	ldx 	#10
-.spacer2
-	lda 	#0
-	sta		PF0
-	sta 	PF1
-	sta		PF2
-	sta 	WSYNC
-	
-	dex
-	sta 	WSYNC
-	bne 	.spacer2
-
-	ldx		#10
-.drawIsland3
-	lda 	IslandSprite
-	sta 	PF1
-	sta 	PF2
-	lda 	#0
-	sta 	PF0
-	lda 	#IslandColors-1,x
-	sta 	COLUPF
-	sta 	WSYNC
-	
-	dex
-	sta 	WSYNC
-	bne 	.drawIsland3
-	
-	ldx 	#10
-.spacer3
-	lda 	#0
-	sta		PF0
-	sta 	PF1
-	sta		PF2
-	sta 	WSYNC
-	
-	dex
-	sta 	WSYNC
-	bne 	.spacer3
-	
-	ldx 	#10
-.drawIsland
-	lda 	IslandSprite
-	sta 	PF1
-	sta 	PF2
-	lda 	#0
-	sta 	PF0
-	lda 	#IslandColors-1,x
-	sta 	COLUPF
-	sta 	WSYNC
-	
-	dex
-	sta 	WSYNC
-	bne 	.drawIsland
 	; original is 42
-	ldx 	#2
+	ldx 	#60
 ; Bottom part - score will go here
 .finalspacer
 	lda 	#0
@@ -275,6 +566,49 @@ Spacer
 	sta		WSYNC
 	bne		.wsynca
 	rts
+	
+drawIslands
+	lda		#ISLAND_ROWS ; 3
+	sta		islandrows
+.beginRow
+	lda 	#ISLAND_HEIGHT ; 9
+	sta 	drawisland
+.startDrawIsland
+	lda 	IslandSprite
+	sta 	PF1
+	sta 	PF2
+	lda 	#0
+	sta 	PF0
+	lda 	#IslandColors-1,x
+	sta 	COLUPF
+;	sta 	WSYNC
+
+; the zero flag isn't being set????/
+	dec		drawisland
+	cmp 	#$FF
+	sta		WSYNC
+	bne 	.startDrawIsland
+	
+drawSpacer
+	lda		#SPACER_HEIGHT
+	sta 	spacerheight
+.startDrawSpacer
+	lda 	#0
+	sta		PF0
+	sta 	PF1
+	sta		PF2
+	
+	dec		spacerheight
+	cmp 	#$FF
+	sta		WSYNC
+	bne 	.startDrawSpacer
+	
+	dec 	islandrows
+	cmp		#$FF
+	bne		.beginRow
+	rts
+
+
 
 ;------------------------------------------------
 ; ROM Tables
@@ -283,6 +617,21 @@ Spacer
 ;---Graphics Data from PlayerPal 2600---
 
 IslandSprite
+		.byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+		.byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
+        .byte #%01111110
         .byte #%01111110
         .byte #%01111110
         .byte #%01111110
@@ -309,6 +658,21 @@ IslandColors
         .byte #$C8;
         .byte #$C8;
         .byte #$C8;
+		.byte #$C8;
+        .byte #$C8;
+        .byte #$C8;
+        .byte #$C8;
+        .byte #$C8;
+        .byte #$C8;
+        .byte #$C8;
+        .byte #$C8;
+        .byte #$C8;
+		.byte #$C8;
+		.byte #$C8;
+        .byte #$C8;
+        .byte #$C8;
+        .byte #$C8;
+		.byte #$C8;
 ;---End Color Data---
 
 ;---Graphics Data from PlayerPal 2600---
